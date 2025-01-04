a259937434057cb78073070c43ba7f905431539b
lokranjanp <lokranjan03@gmail.com>
2025-01-04 11:44:47 +0530

    use flask approach, migrate to mongo and implementing connection pooling

--- authcode/__init__.py
+++
@@ -1,6 +0,0 @@
-# authcode/__init__.py
-
-from .otp import *  # Import everything from otp.py
-from .register import *  # Import everything from register.py
-from .sessions import *  # Import everything from sessions.py
-from .otpmail import *  # Import everything from otpmail.py
